de63d3c78b74a2a5d033f30d8a0053b3b11be8f4
shenmengzhao <shenmengzhao@wancloud.io>
2021-04-20 17:45:23 +0800

    fix(router): fix router import

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: import('@/views/home.vue'),
+    component: () => import('@/views/home.vue'),
   },
 ]
 
